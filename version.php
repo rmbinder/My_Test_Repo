ae58bf67fbfe31a70d98115531998556eea5ab1a
rmbinder <r-m-b@gmx.net>
2016-11-05 23:39:34 +0100

    version changed

--- version.php
+++ version.php
@@ -7,15 +7,8 @@
  * @see http://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  * 
-<<<<<<< HEAD
  ***********************************************************************************************
  */
-=======
- ****************************************************************************/
-
-$plugin_version = '2.0.0a';
-$plugin_stand   = '31.03.2016';
->>>>>>> Testzweig_ab_2.0.0
 
 $plugin_version = '2.1.0';
 $plugin_stand   = '09.02.2016';
